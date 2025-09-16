5069104072f714425ad4c7305e0cc761d70f83bb
gleb <glebdremin@mail.ru>
2025-09-16 12:00:52 +0300

    111

---
+++ PCS.1tetrad/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float one, two, result;
+            char sign;
+
+            Console.WriteLine("Добро пожаловать в калькулятор.");
+            Console.WriteLine("Вам необходимо ввести первое число, затем знак действия (+, -, *, /), и второе число.");
+
+            Console.Write("Введите первое число: ");
+            one = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Введите знак действия: ");
+            sign = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            two = Convert.ToSingle(Console.ReadLine());
+
+            if (sign == '+')
+            {
+                result = one + two;
+                Console.WriteLine("Сумма ваших чисел равна " + result);
+            }
+            else if (sign == '-')
+            {
+                result = one - two;
+                Console.WriteLine("Разность ваших чисел равна " + result);
+            }
+            else if (sign == '*')
+            {
+                result = one * two;
+                Console.WriteLine("Произведение ваших чисел равно " + result);
+            }
+            else if (sign == '/')
+            {
+                if (two == 0)
+                {
+                    Console.WriteLine("Ошибка. Делитель не может быть равен нулю.");
+                }
+                else
+                {
+                    result = one / two;
+                    Console.WriteLine("Частное ваших чисел равно " + result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+            }
+
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
--- PCS.1tetrad/obj/Debug/net9.0/PCS.1tetrad.AssemblyInfo.cs
+++ PCS.1tetrad/obj/Debug/net9.0/PCS.1tetrad.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PCS.1tetrad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9cf641741dbc95fdb3299e22bef17b4d756ab5")]
+[assembly: System.Reflection.AssemblyProductAttribute("PCS.1tetrad")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PCS.1tetrad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
